8b172996c91e605109553f2f6c7c58d38ac25a04
Anna Dai <adai@student.ethz.ch>
2019-06-04 09:48:25 +0100

    map to world

--- include/supereight_ros/functions.hpp
+++ include/supereight_ros/functions.hpp
@@ -15,8 +15,9 @@ namespace se {
  * @param[in] depth_msg destination of depth image
  */
 
-void ConvertDisp2Depth(const sensor_msgs::ImageConstPtr &disp_msg,
-                                          sensor_msgs::ImagePtr &depth_msg, const float &constant) {
+void convertDisp2Depth(const sensor_msgs::ImageConstPtr &disp_msg,
+                       sensor_msgs::ImagePtr &depth_msg,
+                       const float &constant) {
   // for each depth Z = focal length*baseline/disparity
 
   const float *disp_row = reinterpret_cast<const float *>(&disp_msg->data[0]);
@@ -39,7 +40,8 @@ void ConvertDisp2Depth(const sensor_msgs::ImageConstPtr &disp_msg,
  * @param old_image_msg
  * @param new_image_msg
  */
-void CreateImageMsg(const sensor_msgs::ImageConstPtr &old_image_msg, sensor_msgs::ImagePtr &new_image_msg){
+void createImageMsg(const sensor_msgs::ImageConstPtr &old_image_msg,
+                    sensor_msgs::ImagePtr &new_image_msg) {
   new_image_msg->header = old_image_msg->header;
   new_image_msg->width = old_image_msg->width;
   new_image_msg->height = old_image_msg->height;
@@ -53,7 +55,9 @@ void CreateImageMsg(const sensor_msgs::ImageConstPtr &old_image_msg, sensor_msgs
  * @param new_image_msg
  * @param image_size
  */
-void CreateImageMsg(const supereight_ros::ImagePose::ConstPtr &old_image_msg, sensor_msgs::ImagePtr &new_image_msg, Eigen::Vector2i image_size){
+void createImageMsg(const supereight_ros::ImagePose::ConstPtr &old_image_msg,
+                    sensor_msgs::ImagePtr &new_image_msg,
+                    Eigen::Vector2i image_size) {
   new_image_msg->header = old_image_msg->image.header;
   new_image_msg->width = image_size.x();
   new_image_msg->height = image_size.y();
@@ -61,16 +65,17 @@ void CreateImageMsg(const supereight_ros::ImagePose::ConstPtr &old_image_msg, se
   new_image_msg->data = std::vector<uint8_t>(image_size.x() * image_size.y() * sizeof(float));
 }
 
+
 /**
  * @brief swaps the rotation axis to convert the world frame to octree frame
  * @param[in] camera pose in world frame
  * @return rotated pose
  */
-Eigen::Matrix4f SwapAxes_octree_world(const Eigen::Matrix4f &input){
+Eigen::Matrix4f swapAxes(const Eigen::Matrix4f &input) {
   Eigen::Matrix4f output = input;
-  output.block<3,1>(0,0) = -input.block<3,1>(0,1);
-  output.block<3,1>(0,1) = -input.block<3,1>(0,2);
-  output.block<3,1>(0,2) = input.block<3,1>(0,0);
+  output.block<3, 1>(0, 0) = -input.block<3, 1>(0, 1);
+  output.block<3, 1>(0, 1) = -input.block<3, 1>(0, 2);
+  output.block<3, 1>(0, 2) = input.block<3, 1>(0, 0);
   return output;
 }
 
