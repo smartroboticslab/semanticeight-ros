7bfd17527060991799ac258af4dfa3251462f78f
Anna Dai <adai@student.ethz.ch>
2019-06-04 09:43:16 +0100

    comment  compile flags out

--- src/supereight_ros_node.cpp
+++ src/supereight_ros_node.cpp
@@ -8,19 +8,19 @@ int main(int argc, char **argv) {
   ros::NodeHandle nh;
   ros::NodeHandle nh_private("~");
 
-#ifdef MAP_OM
+//#ifdef MAP_OM
   SupereightNode<OFusion> node(nh, nh_private);
   ROS_INFO_STREAM("Occupancy map");
-#elif MAP_SDF
-  SupereightNode<SDF> node(nh, nh_private);
-  ROS_INFO_STREAM("SDF map");
-#endif
+//#elif MAP_SDF
+//  SupereightNode<SDF> node(nh, nh_private);
+//  ROS_INFO_STREAM("SDF map");
+//#endif
 
   std::cout << "FINISHED" << std::endl;
 
   ros::spin();
 
-  Stats.print_all_data(std::cout, false);
+//  Stats.print_all_data(std::cout, false);
   // delete pipeline;
   return 0;
 }
