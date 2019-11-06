ad8503187b65ddeb0e13e1dd2f747e3b90f29bde
Sotiris Papatheodorou <sotirisp@protonmail.com>
2019-11-06 19:53:29 +0000

    Improve coding style of utilities

--- include/supereight_ros/utilities.hpp
+++ include/supereight_ros/utilities.hpp
@@ -5,36 +5,50 @@
 #ifndef __UTILITIES_HPP
 #define __UTILITIES_HPP
 
-#include "supereight_ros/supereight_ros.hpp"
+#include <Eigen/Dense>
+
+#include <sensor_msgs/Image.h>
+
+#include "supereight_ros/ImagePose.h"
+
+
 
 namespace se {
-
-/**
- * @brief create new image message with all attributes
- * @param old_image_msg
- * @param new_image_msg
- */
-void createImageMsg(const sensor_msgs::ImageConstPtr &old_image_msg,
-                    sensor_msgs::ImagePtr &new_image_msg);
-/**
- * @brief create new image message with all attributes
- * @param old_image_msg
- * @param new_image_msg
- * @param image_size
- */
-void createImageMsg(const supereight_ros::ImagePose::ConstPtr &old_image_msg,
-                    sensor_msgs::ImagePtr &new_image_msg,
-                    Eigen::Vector2i image_size);
+  /**
+   * @brief create new image message with all attributes
+   * @param old_image_msg
+   * @param new_image_msg
+   */
+  void createImageMsg(const sensor_msgs::ImageConstPtr& old_image_msg,
+                      sensor_msgs::ImagePtr&            new_image_msg);
 
 
-/**
- * @brief swaps the rotation axis to convert the world frame to octree frame
- * @param[in] camera pose in world frame
- * @return rotated pose
- */
-Eigen::Matrix4f swapAxes(const Eigen::Matrix4f &input);
 
-void createTestImage(const sensor_msgs::ImageConstPtr &disp_msg,
-                       sensor_msgs::ImagePtr &depth_msg);
-}
-#endif //EXPLORATION_WS_FUNCTIONS_HPP
+  /**
+   * @brief create new image message with all attributes
+   * @param old_image_msg
+   * @param new_image_msg
+   * @param image_size
+   */
+  void createImageMsg(const supereight_ros::ImagePose::ConstPtr& old_image_msg,
+                      sensor_msgs::ImagePtr&                     new_image_msg,
+                      Eigen::Vector2i&                           image_size);
+
+
+
+  /**
+   * @brief swaps the rotation axis to convert the world frame to octree frame
+   * @param[in] camera pose in world frame
+   * @return rotated pose
+   */
+  Eigen::Matrix4f swapAxes(const Eigen::Matrix4f& input);
+
+
+
+  void createTestImage(const sensor_msgs::ImageConstPtr& disp_msg,
+                       sensor_msgs::ImagePtr&            depth_msg);
+
+} // namespace se
+
+#endif
+
--- src/utilities.cpp
+++ src/utilities.cpp
@@ -3,93 +3,86 @@
 //
 
 #include "supereight_ros/utilities.hpp"
-#include "supereight_ros/supereight_ros.hpp"
+
+
 
 namespace se {
+  void createImageMsg(const sensor_msgs::ImageConstPtr& old_image_msg,
+                      sensor_msgs::ImagePtr&            new_image_msg) {
 
-/**
- * @brief create new image message with all attributes
- * @param old_image_msg
- * @param new_image_msg
- */
-void createImageMsg(const sensor_msgs::ImageConstPtr &old_image_msg,
-                    sensor_msgs::ImagePtr &new_image_msg) {
-  new_image_msg->header = old_image_msg->header;
-  new_image_msg->width = old_image_msg->width;
-  new_image_msg->height = old_image_msg->height;
-  new_image_msg->step = sizeof(float) * new_image_msg->width;
-  new_image_msg->encoding = old_image_msg->encoding;
-
-}
-/**
- * @brief create new image message with all attributes
- * @param old_image_msg
- * @param new_image_msg
- * @param image_size
- */
-void createImageMsg(const supereight_ros::ImagePose::ConstPtr &old_image_msg,
-                    sensor_msgs::ImagePtr &new_image_msg,
-                    Eigen::Vector2i image_size) {
-  new_image_msg->header = old_image_msg->image.header;
-  new_image_msg->width = image_size.x();
-  new_image_msg->height = image_size.y();
-  new_image_msg->step = sizeof(float) * image_size.x(); //sizeof(float)=4
-  new_image_msg->data = std::vector<uint8_t>(image_size.x() * image_size.y() * sizeof(float));
-}
+    new_image_msg->header       = old_image_msg->header;
+    new_image_msg->height       = old_image_msg->height;
+    new_image_msg->width        = old_image_msg->width;
+    new_image_msg->encoding     = old_image_msg->encoding;
+    new_image_msg->is_bigendian = old_image_msg->is_bigendian;
+    new_image_msg->step         = sizeof(float) * new_image_msg->width; // TODO fix this hack.
+  }
 
 
-/**
- * @brief swaps the rotation axis to convert the world frame to octree frame
- * @param[in] camera pose in world frame
- * @return rotated pose
- */
-Eigen::Matrix4f swapAxes(const Eigen::Matrix4f &input) {
-  Eigen::Matrix4f output = input;
-  output.block<3, 1>(0, 0) = -input.block<3, 1>(0, 1);
-  output.block<3, 1>(0, 1) = -input.block<3, 1>(0, 2);
-  output.block<3, 1>(0, 2) = input.block<3, 1>(0, 0);
-  return output;
-}
 
-void createTestImage(const sensor_msgs::ImageConstPtr &disp_msg,
-                       sensor_msgs::ImagePtr &depth_msg){
+  void createImageMsg(const supereight_ros::ImagePose::ConstPtr& old_image_msg,
+                      sensor_msgs::ImagePtr&                     new_image_msg,
+                      Eigen::Vector2i&                           image_size) {
 
-  int row_step = depth_msg->step / sizeof(float);
-  float *depth_data = reinterpret_cast<float *>(&depth_msg->data[0]);
-  for (int v = 0; v < (int) depth_msg->height; ++v) {
-    for (int u = 0; u < (int) depth_msg->width; ++u) {
+    new_image_msg->header = old_image_msg->image.header;
+    new_image_msg->height = image_size.y();
+    new_image_msg->width  = image_size.x();
+    new_image_msg->step   = sizeof(float) * image_size.x(); //sizeof(float)=4 // TODO fix this hack.
+    new_image_msg->data   = std::vector<uint8_t>(image_size.x() * image_size.y() * sizeof(float));
+  }
+
+
+
+  Eigen::Matrix4f swapAxes(const Eigen::Matrix4f& input) {
+    Eigen::Matrix4f output = input;
+    output.block<3, 1>(0, 0) = -input.block<3, 1>(0, 1);
+    output.block<3, 1>(0, 1) = -input.block<3, 1>(0, 2);
+    output.block<3, 1>(0, 2) =  input.block<3, 1>(0, 0);
+    return output;
+  }
+
+
+
+  void createTestImage(const sensor_msgs::ImageConstPtr& disp_msg,
+                       sensor_msgs::ImagePtr&            depth_msg) {
+
+    const int row_step = depth_msg->step / sizeof(float);
+    float *depth_data = reinterpret_cast<float *>(&depth_msg->data[0]);
+    for (int v = 0; v < (int) depth_msg->height; ++v) {
+      for (int u = 0; u < (int) depth_msg->width; ++u) {
         *depth_data = 3.0;
         ++depth_data;
+      }
     }
-  }
-  int ncol = 300;
-   float *depth_data2 = reinterpret_cast<float *>(&depth_msg->data[ncol + 80 * row_step]);
-  for (int h = 80; h < (int) depth_msg->height; h++){
-    for (int w = ncol; w < ncol + 100 ;w++){
-      *depth_data2= 5.8;
-      ++depth_data2;
+    int ncol = 300;
+    float *depth_data2 = reinterpret_cast<float *>(&depth_msg->data[ncol + 80 * row_step]);
+    for (int h = 80; h < (int) depth_msg->height; h++){
+      for (int w = ncol; w < ncol + 100 ;w++){
+        *depth_data2= 5.8;
+        ++depth_data2;
+      }
+      depth_data2 = depth_data2 + row_step- 100;
     }
-    depth_data2 = depth_data2 + row_step- 100;
+
+    ncol = 900;
+    float *depth_data3 = reinterpret_cast<float *>(&depth_msg->data[ncol + 280 * row_step]);
+    for(int h = 280 ; h < (int) depth_msg->height ; h ++){
+      for (int w = ncol; w < ncol + 150; w++){
+        *depth_data3 = 1.f;
+        ++depth_data3;
+      }
+      depth_data3=depth_data3 + row_step - 150;
+    }
+    ncol = 1600;
+    float *depth_data4 = reinterpret_cast<float *>(&depth_msg->data[ncol + 380 * row_step]);
+    for(int h = 380 ; h < (int) depth_msg->height ; h ++){
+      for (int w = ncol; w < ncol + 150; w++){
+        *depth_data4 = 0.f;
+        ++depth_data4;
+      }
+      depth_data4=depth_data4 + row_step - 150;
+    }
   }
 
-  ncol = 900;
-  float *depth_data3 = reinterpret_cast<float *>(&depth_msg->data[ncol + 280 * row_step]);
-  for(int h = 280 ; h < (int) depth_msg->height ; h ++){
-    for (int w = ncol; w < ncol + 150; w++){
-      *depth_data3 = 1.f;
-      ++depth_data3;
-    }
-    depth_data3=depth_data3 + row_step - 150;
-  }
-  ncol = 1600;
-  float *depth_data4 = reinterpret_cast<float *>(&depth_msg->data[ncol + 380 * row_step]);
-  for(int h = 380 ; h < (int) depth_msg->height ; h ++){
-    for (int w = ncol; w < ncol + 150; w++){
-      *depth_data4 = 0.f;
-      ++depth_data4;
-    }
-    depth_data4=depth_data4 + row_step - 150;
-  }
-}
-}
+} // namespace se
 
