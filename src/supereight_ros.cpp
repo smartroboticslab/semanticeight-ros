c4ad37134aa54762dec88f62cf1ed9478bb566dc
Sotiris Papatheodorou <sotiris@papatheodorou.xyz>
2022-07-01 16:26:17 +0100

    Update real-world experiment semantic classes

--- src/supereight_ros.cpp
+++ src/supereight_ros.cpp
@@ -300,8 +300,8 @@ SupereightNode::SupereightNode(const ros::NodeHandle& nh, const ros::NodeHandle&
     if (node_config_.dataset == Dataset::Real) {
         se::semantic_classes.setEnabled("backpack");
         se::semantic_classes.setEnabled("book");
-        se::semantic_classes.setEnabled("bottle");
         se::semantic_classes.setEnabled("cup");
+        se::semantic_classes.setEnabled("keyboard");
     }
     else {
         se::semantic_classes.setEnabled("chair");
