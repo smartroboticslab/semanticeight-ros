a69c58e0aa70112b0fc19c2c3bdd2741dd6c371b
Sotiris Papatheodorou <sotiris@papatheodorou.xyz>
2022-08-19 18:07:59 +0100

    Reduce target resolution for real-world objects
    The effective resolution ends up being around 1cm which is very
    sensible considering the amount of noise in the RealSense D455.

--- src/supereight_ros.cpp
+++ src/supereight_ros.cpp
@@ -307,7 +307,7 @@ SupereightNode::SupereightNode(const ros::NodeHandle& nh, const ros::NodeHandle&
         se::semantic_classes.setEnabled("chair");
     }
     if (node_config_.dataset == Dataset::Real) {
-        se::semantic_classes.setResAll(0.01f);
+        se::semantic_classes.setResAll(0.02f);
     }
     else {
         se::semantic_classes.setResAll(0.01f);
